17e095a68ce2ee296f19351cf7377cb2e2cc99c3
mclarke16 <mick.clarke@hotmail.com>
2023-12-20 18:55:36 +0100

    Update httpServer_c.c

--- httpServer_c.c
+++ httpServer_c.c
@@ -6,8 +6,153 @@
 
 
 #define PORT 8080
+#define BUFFER_SIZE 1024
 
-void *handle_client(void* arg);
+
+const char *get_file_extension(const char *file_name)
+{
+    const char *dot = strrchr(file_name, '.');
+    if(!dot || dot == file_name)
+    {
+        return "";
+    }
+    return dot + 1;
+}
+
+const char *get_mime_type(const char *file_ext)
+{
+    if(strcasecmp(file_ext, "html") == 0 || strcasecmp(file_ext, "htm") == 0)
+    {
+        return "text/html"''
+    }
+    else if(strcasecmp(file_ext, "txt") == 0)
+    {
+        return "text/plain";
+    }
+    else if(strcasecmp(file_ext, "jpg") == 0)
+    {
+        return "image/jpeg";
+    }
+    else if(strcasecmp(file_ext, "png") == 0)
+    {
+        return "image/png";
+    }
+    else 
+    {
+        return "application/octet-stream";
+    }
+}
+
+bool case_insensitive_compare(const char *s1, const char *s2)
+{
+    while(*s1 && *s2)
+    {
+        if(tolower((unsigned char)*s1) != tolower((unsigned char)*s2))
+        {
+            return false;
+        }
+        s1++;
+        s2++;
+    }
+    return *s1 == *s2;
+}
+
+char *get_file_case_insensitive(const char *file_name)
+{
+    DIR *dir = opendir(".");
+    if(dir == NULL)
+    {
+        perror("opendir");
+        return NULL;
+    }
+
+    struct dirent *entry;
+    char *found_file_name = NULL;
+    while((entry == readdir(dir)) != NULL)
+    {
+        if(case_insensitive_compare(entry->d_name, file_name))
+        {
+            found_file_name = entry->d_name;
+            break;
+        }
+    }
+    closedir(dir);
+    return found_file_name;
+}
+
+char *url_decode(const char *src)
+{
+    size_t src_len = strlen(src);
+    char *decoded = malloc(src_len + 1);
+    size_t decoded_len = 0;
+
+    // decode %2x to hex
+    for(size_t i = 0; i < src_len; i++)
+    {
+        if(src[i] == '%' && i + 2 < src_len)
+        {
+            int hex_val;
+            sscanf(src + i + 1, "%2x", &hex_val);
+            decoded[decoded_len++] = hex_val;
+            i += 2;
+        }
+        else
+        {
+            decoded[decoded_len++] = src[i];
+        }
+    }
+
+    // add null terminator
+
+    decoded[decoded_len] = '\0';
+    return decoded;
+}
+
+void build_http_response(const char *file_name, const char *file_ext, char *response, size_t *response_len)
+{
+    // build HTTP header
+    const char *mime_type = get_mime_type(file_ext);
+    char *header = (char *)malloc(BUFFER_SIZE * sizeof(char));
+    snprintf(header, BUFFER_SIZE,
+        "HTTP/1.1 200 OK\r\n"
+        "Content-Type %s\r\n"
+        "\r\n",
+        mime_type);
+
+    // if file does not exist, respond with 404 Not Found
+    int file_fd = open(file_name, O_RDONLY);
+    if(file_fd == -1)
+    {
+        snprintf(header, BUFFER_SIZE,
+        "HTTP/1.1 404 Not Found\r\n"
+        "Content-Type: text/plain\r\n"
+        "\r\n"
+        "404 Not Found");
+        *response_len = strlen(response);
+        return;
+    }
+
+    // get file size for Content-Length
+    struct stat file_stat;
+    fstat(file_fd, &file_stat);
+    off_t file_size = file_stat.st_size;
+
+    // copy header to response buffer
+    *response_len = 0;
+    memcpy(response, header, strlen(header));
+    *response_len += strlen(header);
+
+    // copy file to response buffer
+    ssize_t bytes_read;
+    while((bytes_read = read(file_fd, response + *response_len, BUFFER_SIZE - *response_len)) > 0)
+    {
+        *response_len += bytes_read;
+    }
+    free(header);
+    close(file_fd);
+}
+
+
 
 int main(int argc, char* argv[])
 {
@@ -38,6 +183,7 @@ int main(int argc, char* argv[])
         perror("listen failed");
         exit(EXIT_FAILURE);
     }
+
     while(1)
     {
         //client info
@@ -56,9 +202,14 @@ int main(int argc, char* argv[])
         pthread_create(&thread_id, NULL, handle_client, (void *)client_fd);
         pthread_detach(thread_id);
     }
+
+
+
+
+
+    
     return 0;
 }
-
 
 void *handle_client(void* arg)
 {
